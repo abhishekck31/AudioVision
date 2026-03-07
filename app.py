4b15a69c4c28e5ab7e0bbeef16cccea9e91526a0
Abhishek C K <abhishekkannur31@gmail.com>
2026-03-07 14:48:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 st.title("AudioVision - Audio Feature Visualization")
+st.audio(uploaded_file)
 
 uploaded_file = st.file_uploader("Upload an audio file", type=["wav"])
 

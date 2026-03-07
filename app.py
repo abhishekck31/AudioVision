efeee7456802a6660b4408ec763b135aa457b3e5
Abhishek C K <abhishekkannur31@gmail.com>
2026-03-07 14:49:21 +0530

    Refactor audio visualization code for clarity

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 import librosa
 import librosa.display
@@ -6,37 +5,49 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 st.title("AudioVision - Audio Feature Visualization")
-st.audio(uploaded_file)
 
 uploaded_file = st.file_uploader("Upload an audio file", type=["wav"])
 
 if uploaded_file is not None:
-    
+
+    st.audio(uploaded_file)
+
     signal, sr = librosa.load(uploaded_file)
 
     st.write("Sampling Rate:", sr)
 
     # Waveform
     st.subheader("Waveform")
+
     fig, ax = plt.subplots()
     librosa.display.waveshow(signal, sr=sr, ax=ax)
     st.pyplot(fig)
 
     # Spectrogram
     st.subheader("Spectrogram")
+
     spectrogram = librosa.stft(signal)
     spectrogram_db = librosa.amplitude_to_db(abs(spectrogram))
 
     fig, ax = plt.subplots()
-    img = librosa.display.specshow(spectrogram_db, sr=sr, x_axis="time", y_axis="hz", ax=ax)
+    img = librosa.display.specshow(
+        spectrogram_db,
+        sr=sr,
+        x_axis="time",
+        y_axis="hz",
+        ax=ax
+    )
+
     fig.colorbar(img, ax=ax)
     st.pyplot(fig)
 
     # MFCC
     st.subheader("MFCC Features")
+
     mfcc = librosa.feature.mfcc(y=signal, sr=sr, n_mfcc=13)
 
     fig, ax = plt.subplots()
     img = librosa.display.specshow(mfcc, x_axis="time", ax=ax)
+
     fig.colorbar(img, ax=ax)
     st.pyplot(fig)
